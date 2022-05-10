74ba97bab34ec80873ab5056cfcdb10e26da3643
Spencer Kerans <skerans0@gmail.com>
2022-05-10 09:26:36 -0600

    fixed e.split is no a func

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,7 +29,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get('content');
   const result = await request;
   console.log('result.value', result);
   return result;
